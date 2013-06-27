dff1d613ec591130506692daeea7d82bd95f978d
John Webster <jlwebster@gmail.com>
2013-06-27 12:08:55 -0600

    Initial commit

---
+++ GitTraining/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTraining
+//
+//  Created by John Webster on 6/27/13.
+//  Copyright (c) 2013 John Webster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTraining/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitTraining
+//
+//  Created by John Webster on 6/27/13.
+//  Copyright (c) 2013 John Webster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
